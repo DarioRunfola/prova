a77a7b3b492712e5fce0b40822cf2f83b2a126fa
Dario Runfola <85560846+DarioRunfola@users.noreply.github.com>
2022-02-10 12:28:02 +0100

    incompleto

---
+++ correzioneTodo/toodolist.js
@@ -0,0 +1,30 @@
+$(() => {
+    let mioAggiungo = [];
+
+    init()
+
+    function init() {
+        scriviLista();
+    }
+
+    function scriviLista() {
+        let lista = '';
+        for (i = 0; i < mioAggiungo.length; i++) {
+            lista += '<li class="list-group-item" >' + mioAggiungo[i] + '<button class="btn1" type="button">' + "X" + '</button>' + '</li>'
+        }
+        $('#aggiungi').html(lista);
+    }
+
+
+    $('#btn').on('click', function () {
+        let nuovoElemento = $('#nuovoElemento').val();
+        mioAggiungo.push(nuovoElemento);
+        scriviLista();
+    });
+
+    $(document).on('click', '.btn1', function () {
+        let indice = $(this.parentElement).index();
+        mioAggiungo.splice(indice, 1);
+        scriviLista();
+    })
+})
