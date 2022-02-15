a3800128ba2afc5b0210c0041b419427b0210bc0
Dario Runfola <85560846+DarioRunfola@users.noreply.github.com>
2022-02-15 12:40:05 +0100

    Update prove.js

--- correzioneTodo/ToDoLIst/js/prove.js
+++ correzioneTodo/ToDoLIst/js/prove.js
@@ -41,47 +41,40 @@ import { Todo } from '../js/Todo.js';
 // }
 
 $(() => {
-    let todolist = [];
 
-    stampaTodo();
-
-    function stampaTodo() {
-
-        let button = $('.todo button');
-        button.on('click', function () {
-            let titolo = $('#titolo');
-            let testo = $('#testo');
-            let todo = new Todo(titolo.value, testo.value);
-            todolist.push(todo);
-            localStorage.setItem('listaTodo', JSON.stringify(todolist));
-            stampaTodo();
-
-        });
-
-    }
     let stampaTodo = () => {
         let lista = $('.lista ul');
-        lista.innerHTML = '';
+        lista.html('');
 
         let localLista = localStorage.getItem('listaTodo');
         if (localLista !== null) {
             todolist = JSON.parse(localLista);
+            console.log(todolist);
         }
 
-       $(todolist).each('todo', '.rimuovi' ,function() {
-            let li = $.create('li');
-            let i = 0;
-            li.className = 'list-group-item';
-            li.innerHTML = todo.titolo + ' - ' + todo.testo;
-            li.innerHTML += '<span class="rimuovi" class="btn btn-sm btn-danger float-end">X</span>';
-            lista.appendChild(li);
-            let rimuovi = $('.rimuovi');
-            rimuovi.on('click', '.rimuovi', function () {
-                todolist.splice(todolist.indexOf(todo), 1);
+        $.each(todolist, function (i, todo) {
+            let li = $('<li></li>');
+            li.addClass('list-group-item');
+            li.html(todo.titolo + ' - ' + todo.testo);
+            li.append('<span class="btn btn-sm btn-danger float-end">X</span>');
+            lista.append(li);
+            li.children().on('click', function () {
+                todolist.splice(i, 1);
                 localStorage.setItem('listaTodo', JSON.stringify(todolist));
                 stampaTodo();
             })
         });
     }
 
-});
+
+    stampaTodo();
+
+    $('.todo button').on('click', function () {
+        let titolo = $('#titolo').val();
+        let testo = $('#testo').val();
+        let todo = new Todo(titolo, testo);
+        todolist.push(todo);
+        localStorage.setItem('listaTodo', JSON.stringify(todolist));
+        stampaTodo();
+
+    });
